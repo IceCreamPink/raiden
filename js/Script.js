d6c3d3c034e964d39cfc07df058a4da56d877dfb
IceCreamPink <henradwi1@gmail.com>
2023-08-16 17:09:43 +0700

    first commit

---
+++ js/Script.js
@@ -0,0 +1,9 @@
+let menu = (document.querySelector = "#menu-icon");
+let navlis = (document.querySelector = "
+
+navlis");
+
+menu.onclick = () => {
+  menu.classlist.toggle("bx-menu");
+  navlis.classlist.toggle("open");
+};
